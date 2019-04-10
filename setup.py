f35ca3b8e4e0df0bbda313599bdbb34381c7a374
Parakoopa <parakoopa@live.de>
2019-04-11 00:46:39 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,19 @@ setup(
     version='0.1',
     packages=find_packages(),
     description='TODO',  # TODO
-    long_description='TODO',  # TODO
+    long_description='TODO - Project will be available starting May/June',  # TODO
     install_requires=[
         'riptide_lib == 0.1',
         'schema >= 0.6'
     ],
     # TODO
     classifiers=[
+        'Development Status :: 4 - Beta',
         'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     entry_points='''
         [riptide.db_driver]
