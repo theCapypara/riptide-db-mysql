bf5fa9aee913698aad9f1bb8d68800a83b4d2b8b
Marco Köpcke <marco.koepcke@tudock.de>
2019-10-24 12:21:27 +0200

    Version bump to 0.3.1 -- Fixed requirements.txt issue

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='riptide-db_mysql',
-    version='0.3.0',
+    version='0.3.1',
     packages=find_packages(),
     description='Tool to manage development environments for web applications using containers - MySQL Database Drvier',
     long_description=long_description,
