466c7ed44d1a4d2c566ed5bf7a5394ef3d1e6dd3
Marco Köpcke <marco.koepcke@tudock.de>
2021-04-08 16:14:12 +0200

    Migrate from Jenkins to Github Actions

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+__version__ = '0.5.0'
 from setuptools import setup, find_packages
 
 # README read-in
@@ -9,7 +10,7 @@ with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='riptide-db_mysql',
-    version='0.5.0',
+    version=__version__,
     packages=find_packages(),
     description='Tool to manage development environments for web applications using containers - MySQL Database Drvier',
     long_description=long_description,
@@ -27,6 +28,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     entry_points='''
         [riptide.db_driver]
